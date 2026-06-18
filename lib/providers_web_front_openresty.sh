6bcda2c02ee7fe6c9e3e48310d2d2a548c97277b
Develata <pwh785967547@gmail.com>
2026-06-18 19:20:46 +0800

    fix: avoid gawk include builtin collision

--- lib/providers_web_front_openresty.sh
+++ lib/providers_web_front_openresty.sh
@@ -11,7 +11,7 @@ sbd_ensure_openresty_confd_include() {
   backup_conf="${nginx_conf}.sbd.bak.$(date +%s)"
   cp -f "$nginx_conf" "$backup_conf"
   tmp_conf="$(mktemp "${nginx_conf}.tmp.XXXXXX")"
-  if ! awk -v include="$include_line" '
+  if ! awk -v inc_line="$include_line" '
     function count_delta(s,   i,c,d) {
       d = 0
       for (i = 1; i <= length(s); i++) {
@@ -28,14 +28,14 @@ sbd_ensure_openresty_confd_include() {
         in_http = 1
         depth = count_delta(line)
         print line
-        if (depth == 0 && !inserted) { print include; inserted = 1; in_http = 0 }
+        if (depth == 0 && !inserted) { print inc_line; inserted = 1; in_http = 0 }
         next
       }
       if (in_http) {
         if (line ~ /include[[:space:]]+conf\.d\/\*\.conf;/) inserted = 1
         d = count_delta(line)
         if ((depth + d) == 0 && line ~ /^[[:space:]]*}/ && !inserted) {
-          print include
+          print inc_line
           inserted = 1
         }
         depth += d
